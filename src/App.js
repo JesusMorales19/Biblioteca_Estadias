f701d9cb8c71a37abbc893cff067ac7bd468acaf
JesusMorales19 <jesuhernan232@gmail.com>
2024-06-03 18:23:08 -0600

    prueba2_inicio de project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="bg-gradient-to-r from-green-500 from-10% via-slate-50 via-50% to-red-600 to-80% h-16"><h1>Hola</h1></div>
+    
+
+<div id="controls-carousel" class="relative w-full" data-carousel="static">
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <image src="../public/logo192.png"><h1>Hola</h1></image>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+        <image src="../public/logo192.png"></image>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+        <image src="../public/logo192.png"></image>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+        <image src="../public/logo192.png"></image>
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+         <image src="../public/logo192.png"></image>
+        </div>
     </div>
+    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+            </svg>
+            <span class="sr-only">Previous</span>
+        </span>
+    </button>
+    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+            </svg>
+            <span class="sr-only">Next</span>
+        </span>
+    </button>
+</div>
+
+    </>
   );
 }
-
-export default App;
